8dff083a5ddba1fc71cdb2dd2ccf57f13a8ac608
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-07-12 17:59:21 +0530

    Day_001 Done

---
+++ Day_001/Exercise4.java
@@ -0,0 +1,13 @@
+package Day_001;
+
+public class Exercise4 {
+    public static void main(String args[])
+    {
+    int length=20;
+    int width=10;
+    int area=length*width;
+    int perimeter=2*(length+width);
+    System.out.println("Area:"+area);
+    System.out.println("Perimeter:"+perimeter);
+    }
+}
