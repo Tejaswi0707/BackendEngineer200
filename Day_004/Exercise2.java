d870557b516687006f3de4a4aded0c19b62a4c10
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-07-15 21:05:11 +0530

    Day_004 Done

---
+++ Day_004/Exercise2.java
@@ -0,0 +1,18 @@
+package Day_004;
+
+public class Exercise2 {
+    public static void swap(int a, int b)
+    {
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    public static void main(String args[])
+    {
+        int a=10, b=20;
+        swap(a,b);
+        System.out.println("a="+a+" b="+b);
+    }
+    /* As integer is primitive data type, it stores value rather than reference. Hence, changes made outside the method will not be reflected. Hence, the output does not swap the integers, The output is a=10, b=20. To swap those integers, it is advised to write logic within in the main method itself. */
+    
+}
