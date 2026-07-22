c770297b8a66e016e6b384b44e58cee8f4d1bc37
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-07-22 08:40:34 +0530

    Done

--- exercises/Day_008/Main.java
+++ exercises/Day_008/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String args[])
     {
         Student s1=new Student("Tejaswi");
-        Student s2=new Student("Divya");
 
         Book b1=new Book("Harry Potter");
         Book b2=new Book("Lord of Rings");
