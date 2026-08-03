88a2dd45b4996c10ee0fe1c07beb944020a12fc2
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-08-03 11:15:31 +0530

    Updated' git push

--- exercises/Day_021/Main.java
+++ exercises/Day_021/Main.java
@@ -1,17 +1,20 @@
 package exercises.Day_021;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]){
-        Student s1=new Student("Tejaswi",20);
-        Student s2=new Student("Tejaswi",20);
+        Student s1=new Student("Teju",20);
+        Student s2=new Student("Teju",20);
 
-        System.out.println(s1==s2); 
-        System.out.println(s1.equals(s2)); 
-        System.out.println(s1);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
 
-        String stud1=new String("Tejaswi");
-        String stud2=new String("Tejaswi");
-        System.out.println(stud1==stud2);
-        System.out.println(stud1.equals(stud2));
+        System.out.println("-------------");
+        System.out.println(s1.equals(s2));
+
+        HashSet<Student> set=new HashSet<>();
+        set.add(s1);
+
+        System.out.println(set.contains(s2));
     }
 }
--- exercises/Day_021/Student.java
+++ exercises/Day_021/Student.java
@@ -1,22 +1,25 @@
 package exercises.Day_021;
+import java.util.*;
 
 public class Student {
     private String name;
     private int age;
     Student(String name, int age)
     {
-        this.age=age;
         this.name=name;
+        this.age=age;
     }
 
     @Override
-    public boolean equals(Object obj)
-    {
-        if (obj==null){return false;}
-        if(!(obj instanceof Student)){return false;}
-        Student other=(Student) obj;
-        return this.name.equals(other.name) && this.age==other.age;
+    public boolean equals(Object obj){
+        Student other=(Student)obj;
 
+        return this.name.equals(other.name) && this.age==other.age;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, age);
     }
     
 }
