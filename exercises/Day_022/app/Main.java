2a0caf15669fcc5c0ffebeebef7914e86a5b9610
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-08-05 17:36:04 +0530

    Day_022 Done

---
+++ exercises/Day_022/app/Main.java
@@ -0,0 +1,10 @@
+package exercises.Day_022.app;
+import exercises.Day_022.util.MathUtil;
+
+public class Main{
+    public static void main(String args[])
+    {
+        System.out.println(MathUtil.square(7));
+    }
+}
+
---
+++ exercises/Day_022/util/MathUtil.java
@@ -0,0 +1,8 @@
+package exercises.Day_022.util;
+
+public class MathUtil {
+    public static int square(int number){
+        return number * number;
+    }
+    
+}
