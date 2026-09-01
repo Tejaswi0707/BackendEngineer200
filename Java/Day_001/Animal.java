e286229e7a7a4acf22e7fa7e9de9d0a67132b483
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-09-01 11:25:49 +0530

    Updated

---
+++ Java/Day_001/Animal.java
@@ -0,0 +1,22 @@
+package Java.Day_001;
+//animal class
+/*
+This is a simple class that represents an animal with a name. It has a private field name, and public getter and setter methods to access and modify the name. The class can be used to create instances of animals and manage their names. 
+ */
+
+/**
+ Javadoc comment 
+ */
+
+
+
+public class Animal{
+    private String name;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+}
