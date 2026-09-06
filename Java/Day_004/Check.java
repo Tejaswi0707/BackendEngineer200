26b3dc67f6b81215d99c2c7af8f61ecc56dfdde2
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-09-06 11:25:30 +0530

    Updated

--- Java/Day_004/Check.java
+++ Java/Day_004/Check.java
@@ -2,14 +2,23 @@ package Java.Day_004;
 import java.util.Random;
 
 public class Check {
+    int numEggs=0;
     public Check()
     {
         System.out.println("In constructor");
+        System.out.println(numEggs);
+    }
+
+    public Check(int numEggs)
+    {
+        this.numEggs = numEggs;
+        System.out.println(numEggs);
     }
 
     public static void main(String args[])
     {
         Check c=new Check();
+        Check c1=new Check(50);
         Random r=new Random();
         System.out.println(r.nextInt(10));
     }
