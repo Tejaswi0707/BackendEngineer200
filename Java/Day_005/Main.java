01a604addfbd6363153887397360abbb23dc7e00
Tejaswi Kattegummula <tejaswireddy0707@gmail.com>
2026-09-08 12:57:02 +0530

    Updated

--- Java/Day_005/Main.java
+++ Java/Day_005/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String args[])
     {
+        Main m=new Main();
         System.out.println("Features");
+    }
+    {
+        System.out.println("Snowy");
     }
     
 }
